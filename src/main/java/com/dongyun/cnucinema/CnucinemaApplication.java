cb0303460683dc063d650ea849ca42f8b3199b53
Dongyun <dyunzero@gmail.com>
2022-05-09 13:52:03 +0900

    first: CNU Cinema 프로젝트 추가

---
+++ src/main/java/com/dongyun/cnucinema/CnucinemaApplication.java
@@ -0,0 +1,13 @@
+package com.dongyun.cnucinema;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class CnucinemaApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(CnucinemaApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/dongyun/cnucinema/CnucinemaApplicationTests.java
@@ -0,0 +1,13 @@
+package com.dongyun.cnucinema;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class CnucinemaApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
