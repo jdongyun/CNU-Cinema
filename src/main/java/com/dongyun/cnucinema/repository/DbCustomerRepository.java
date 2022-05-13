23285e05346c0b735fdb08eaa1df4edba2dc279e
Dongyun <dyunzero@gmail.com>
2022-05-13 16:12:57 +0900

    fix: 사용자 객체에 권한이 제대로 추가되지 않는 문제 수정

--- src/main/java/com/dongyun/cnucinema/repository/DbCustomerRepository.java
+++ src/main/java/com/dongyun/cnucinema/repository/DbCustomerRepository.java
@@ -1,13 +1,12 @@
 package com.dongyun.cnucinema.repository;
 
-import com.dongyun.cnucinema.spec.entity.Authority;
 import com.dongyun.cnucinema.spec.entity.Customer;
 import com.dongyun.cnucinema.spec.enums.Sex;
 import com.dongyun.cnucinema.spec.repository.CustomerRepository;
-import org.springframework.jdbc.core.JdbcTemplate;
+import org.springframework.jdbc.core.ResultSetExtractor;
 import org.springframework.jdbc.core.RowMapper;
 import org.springframework.jdbc.core.namedparam.MapSqlParameterSource;
-import org.springframework.jdbc.core.simple.SimpleJdbcInsert;
+import org.springframework.jdbc.core.namedparam.NamedParameterJdbcTemplate;
 import org.springframework.security.core.authority.SimpleGrantedAuthority;
 import org.springframework.stereotype.Repository;
 
@@ -18,54 +17,82 @@ import java.util.*;
 @Repository
 public class DbCustomerRepository implements CustomerRepository {
 
-    private final JdbcTemplate jdbcTemplate;
+    private final NamedParameterJdbcTemplate jdbcTemplate;
 
     public DbCustomerRepository(DataSource dataSource) {
-        this.jdbcTemplate = new JdbcTemplate(dataSource);
+        this.jdbcTemplate = new NamedParameterJdbcTemplate(dataSource);
     }
 
     @Override
     public Optional<Customer> findByUsername(String username) {
-        List<Customer> result = jdbcTemplate.query(
-                "select * from Customer join Authority A on Customer.username = A.username where Customer.username = ?",
-                customerRowWithAuthoritiesMapper(), username);
-
+        String sql = "select * from Customer left outer join Authority A on Customer.username = A.username where Customer.username = :username";
+        List<Customer> result = jdbcTemplate.query(sql,
+                new MapSqlParameterSource("username", username),
+                customerRowMapper());
 
         return result.stream().findAny();
     }
 
     @Override
     public List<Customer> findAll() {
-        return jdbcTemplate.query("select * from Customer", customerRowMapper());
+        String sql = "select * from Customer left outer join Authority A on Customer.username = A.username";
+        return jdbcTemplate.query(sql, customerRowExtractor());
     }
 
     @Override
     public void save(Customer customer) {
-        SimpleJdbcInsert customerInsert = new SimpleJdbcInsert(jdbcTemplate);
-        customerInsert.withTableName("Customer");
+        // Customer 생성.
+        String sql = "INSERT INTO Customer (username, name, password, email, birth_date, sex) " +
+                "VALUES (:username, :name, :password, :email, :birth_date, :sex) ON DUPLICATE KEY UPDATE " +
+                "name = :name, password = :password, email = :email, birth_date = :birth_date, sex = :sex";
+        Map<String, Object> params = new HashMap<>();
+        params.put("username", customer.getUsername());
+        params.put("name", customer.getName());
+        params.put("password", customer.getEncryptedPassword());
+        params.put("email", customer.getEmail());
+        params.put("birth_date", customer.getBirthDate().toString());
+        params.put("sex", String.valueOf(customer.getSex()));
 
-        Map<String, Object> parameters = new HashMap<>();
-        parameters.put("username", customer.getUsername());
-        parameters.put("name", customer.getName());
-        parameters.put("password", customer.getEncryptedPassword());
-        parameters.put("email", customer.getEmail());
-        parameters.put("birth_date", customer.getBirthDate());
-        parameters.put("sex", String.valueOf(customer.getSex()));
+        jdbcTemplate.update(sql, params);
 
-        customerInsert.execute(new MapSqlParameterSource(parameters));
+        // Authority 생성. 기본값은 USER.
+        params = new HashMap<>();
+        params.put("username", customer.getUsername());
+        params.put("authority_name", "USER");
 
-        // 기본 권한은 USER.
-        SimpleJdbcInsert authorityInsert = new SimpleJdbcInsert(jdbcTemplate);
-        authorityInsert.withTableName("Authority");
-
-        parameters = new HashMap<>();
-        parameters.put("username", customer.getUsername());
-        parameters.put("authority_name", "USER");
-
-        authorityInsert.execute(new MapSqlParameterSource(parameters));
+        jdbcTemplate.update("INSERT INTO Authority (username, authority_name) " +
+                "VALUES (:username, :authority_name) ON DUPLICATE KEY UPDATE " +
+                "authority_name = :authority_name", params);
     }
-    
-    private RowMapper<Customer> customerRowWithAuthoritiesMapper() {
+
+    private ResultSetExtractor<List<Customer>> customerRowExtractor() {
+        return (rs) -> {
+            List<Customer> customers = new ArrayList<>();
+            if (!rs.next()) return customers;
+
+            while (!rs.isAfterLast()) {
+                Customer customer = Customer.builder()
+                        .username(rs.getString("username"))
+                        .name(rs.getString("name"))
+                        .encryptedPassword(rs.getString("password"))
+                        .email(rs.getString("email"))
+                        .birthDate(LocalDate.parse(rs.getString("birth_date")))
+                        .sex(Sex.valueOf(rs.getString("sex")))
+                        .authorities(new HashSet<>())
+                        .build();
+
+                for (; !rs.isAfterLast(); rs.next()) {
+                    if (!Objects.equals(customer.getUsername(), rs.getString("username"))) break;
+                    customer.getAuthorities().add(new SimpleGrantedAuthority(rs.getString("authority_name")));
+                }
+                customers.add(customer);
+            }
+            return customers;
+        };
+    }
+
+
+    private RowMapper<Customer> customerRowMapper() {
         return (rs, rowNum) -> {
             Customer customer = Customer.builder()
                     .username(rs.getString("username"))
@@ -77,24 +104,11 @@ public class DbCustomerRepository implements CustomerRepository {
                     .authorities(new HashSet<>())
                     .build();
 
-
-            customer.getAuthorities().add(new SimpleGrantedAuthority(rs.getString("authority_name")));
-            while (rs.next()) {
+            for (; !rs.isAfterLast(); rs.next()) {
+                if (!Objects.equals(customer.getUsername(), rs.getString("username"))) break;
                 customer.getAuthorities().add(new SimpleGrantedAuthority(rs.getString("authority_name")));
             }
             return customer;
         };
-    }
-
-    private RowMapper<Customer> customerRowMapper() {
-        return (rs, rowNum) ->
-            Customer.builder()
-                    .username(rs.getString("username"))
-                    .name(rs.getString("name"))
-                    .encryptedPassword(rs.getString("password"))
-                    .email(rs.getString("email"))
-                    .birthDate(LocalDate.parse(rs.getString("birth_date")))
-                    .sex(Sex.valueOf(rs.getString("sex")))
-                    .build();
     }
 }
